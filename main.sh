d141d9a4cc6a897c6a845f5dc2e202e3965b8834
MuhammaD Usama <55662931+MuhammadUsamaMX@users.noreply.github.com>
2023-10-24 06:32:20 +0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -36,7 +36,7 @@ is_root() {
 # Function to install dependencies
 install_dependencies() {
     log "Installing dependencies..."
-     iptables -A INPUT -p tcp --dport 80 -j ACCEPT &&  iptables -A INPUT -p tcp --dport 22 -j ACCEPT &&  iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+    iptables -A INPUT -p tcp --dport 80 -j ACCEPT &&  iptables -A INPUT -p tcp --dport 22 -j ACCEPT &&  iptables -A INPUT -p tcp --dport 443 -j ACCEPT
     sudo apt update
     sudo apt install -y lsb-release curl mysql-client certbot iptables curl wget sudo nano zip ufw
     log "Dependencies installed."
