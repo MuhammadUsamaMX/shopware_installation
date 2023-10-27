a5e27b1e74ec8a1d28a0fcfa3a56cef39b157e28
MuhammaD Usama <55662931+MuhammadUsamaMX@users.noreply.github.com>
2023-10-27 06:41:38 +0500

    Update 1.3.6 zero_trust.sh

--- zero_trust.sh
+++ zero_trust.sh
@@ -130,11 +130,10 @@ check_a_record() {
 check_aaaa_record() {
   if dig +short AAAA "$1" | grep -qE '^[0-9a-fA-F:]+$'; then
     return 0  # AAAA record exists
-  else 
+  else
     return 1  # AAAA record doesn't exist
   fi
 }
-
 
 generate_self_signed_ssl() {
     echo -e "\e[92mGenerating a self-signed SSL certificate...\e[0m"
